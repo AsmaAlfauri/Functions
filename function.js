e0debb5b7b174439782c4a11c39c2600d70b9bc5
asmaa <asmaa.alfauri@gmail.com>
2019-07-09 12:47:33 +0300

    function ex 23-25 solved by me and Rashed

--- function.js
+++ function.js
@@ -452,3 +452,93 @@ function scoreInUniversty(scor) {
     return "A";
 }
 console.log(scoreInUniversty(90))
+
+/*
+23
+Write a function called counter
+that will returns a number bigger
+than the one that returnd before
+and start from 0
+Ex: counter()
+=> 1
+Ex: counter()
+=> 2
+Ex: counter()
+=> 3
+*/
+var x = 0;
+
+function counter() {
+  x = x + 1;
+  return x
+}
+console.log(counter())
+console.log(counter())
+console.log(counter())
+console.log(counter())
+
+
+/*
+24
+Write a function called resetCounter
+that will reset the previuos function
+and return the number before reset and
+a string say that the counter reset
+Ex: counter()
+=> 1
+Ex: counter()
+=> 2
+Ex: counter()
+=> 3
+Ex: resetCounter()
+=> 3 and the counter reset now
+Ex: counter()
+=> 1
+Ex: counter()
+=> 2
+Ex: resetCounter()
+=> 2 and the counter reset now
+Ex: counter()
+=> 1
+*/
+function resetCounter() {
+  var m = x
+  x = 0;
+  return m + " and the counter reset now"
+}
+console.log(resetCounter())
+console.log(counter())
+
+
+/*
+25
+// this question is in progress wait
+Write a function called guessMe
+that will take an argument (number)
+if the number not between 0 and 5 [0,1,2,3,4]
+it will return "Please insert a number between 0 and 5"
+if it is between 0 and 5, and guessed the correct number 0-5 [0,1,2,3,4]
+will return "you guess me correctly"
+if it is between 0 and 5, and not the same number randomly 0-5 [0,1,2,3,4]
+will return "you didnt guess me I was 3"
+Ex: guessMe(0)
+=> "you didnt guess me I was 3"
+Ex: guessMe(10)
+=>"Please insert a number between 0 and 5"
+Ex: guessMe(-5)
+=> "Please insert a number between 0 and 5"
+Ex: guessMe(3)
+=> "you guess me correctly"
+*/
+function gussMe(n) {
+  if (n > 5) {
+    return "Please insert a number between 0 and 5"
+  } else if (n !== 3) {
+    return "you didn't guess me I was 3"
+  } else if (n === 3) {
+    return "you guess me correctly"
+  }
+}
+console.log(gussMe(15))
+console.log(gussMe(3))
+console.log(gussMe(1))
